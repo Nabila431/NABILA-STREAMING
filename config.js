af112d9e4faee853bb669132cbfe40f3ab6403ed
Nabila431 <jesikamahjong@gmail.com>
2025-07-02 08:19:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://vgchzuqtrmohyzojvngw.supabase.co'; // Ganti dengan URL API Anda
+const SUPABASE_URL = 'https://vgchzuqtrmohyzojvngw.supabase.co'; // Ganti dengan URL Supabase Anda
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnY2h6dXF0cm1vaHl6b2p2bmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE1NzAsImV4cCI6MjA2Njg0NzU3MH0.WKcSXto5EXcS1fdScAKf6atW7tcXM9AB9jObapii_2g'; // Ganti dengan kunci Supabase Anda
